c45356065f384c6548cf190fceb92d43f23f6760
hkBeast <himanshu.singh@simplyentertaining.us>
2024-10-16 10:02:03 +0530

    Initial Commit

---
+++ PropertyWrapperExample/PropertyWrapperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrapperExampleApp.swift
+//  PropertyWrapperExample
+//
+//  Created by HKBeast on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrapperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
